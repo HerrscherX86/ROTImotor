4ba721916a79321e774c1b345c9da84d46aeffb0
HerrscherX86 <hafizh1086@gmail.com>
2025-05-15 20:25:29 +0800

    Fix Unittest Abstract func

--- domain/repositories/motor_repository.py
+++ domain/repositories/motor_repository.py
@@ -4,20 +4,25 @@ from domain.entities.motor import Motor
 class MotorRepository(ABC):
     @abstractmethod
     def get_all(self) -> list[Motor]:
-        pass
+        """Return a list of all motors."""
+        pass # pragma: no cover
 
     @abstractmethod
     def get_by_id(self, motor_id: int) -> Motor:
-        pass
+        """Return a single motor by its ID."""
+        pass # pragma: no cover
 
     @abstractmethod
-    def add(self, motor: Motor) -> None:
-        pass
+    def add(self, motor: Motor) -> None: # pragma: no cover
+        """Add a new motor."""
+        pass # pragma: no cover
 
     @abstractmethod
     def update(self, motor: Motor) -> None:
-        pass
+        """Update an existing motor."""
+        pass # pragma: no cover
 
     @abstractmethod
     def delete(self, motor_id: int) -> None:
-        pass
+        """Delete a motor by its ID."""
+        pass # pragma: no cover
